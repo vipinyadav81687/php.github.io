0f687681d3f51575f386b464d0c85cdb41bf9158
vipinyadav81687 <vipinyadav81687@gmail.com>
2026-02-11 09:04:51 +0530

    11feb2026

---
+++ echo.php
@@ -0,0 +1,14 @@
+<?php
+echo "Vipin Yadav";
+echo '<br/>';
+echo 'vipin yadav';
+echo "<br>";
+echo "vipin","yadav";
+echo "<br>";
+echo "vipin "."yadav";
+echo "<br>";
+echo "<b> Vipin Yadav </b>";
+echo "<br>";
+echo 503.2;
+
+?>
---
+++ print.php
@@ -0,0 +1,10 @@
+<?php
+print "VIpin Yadav";
+print"<br>";
+print "vipin"."yadav";
+print "<br>";
+print 80.2;
+print"<br>";
+print "<h1>vipin yadav </h1>";
+// print "vipin" , "yadav";
+?>
