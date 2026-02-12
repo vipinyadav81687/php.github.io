a0fe37352d7e3472224535b551dc1938fb602c02
vipinyadav81687 <vipinyadav81687@gmail.com>
2026-02-12 09:09:34 +0530

    12feb2026

---
+++ variables.php
@@ -0,0 +1,15 @@
+<?php
+$name = "vipin yadav<br>";
+
+// echo $name;
+// echo "<br>";
+// echo $name;
+// echo $name;
+// echo $name;
+// echo $name;
+// echo $name;
+// echo $name;
+$num = 2358;
+echo "How old are you :" . $name; 
+
+?>
