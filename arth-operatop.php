68ddf3531db7599589b1c5b8443970ee090f153b
vipinyadav81687 <vipinyadav81687@gmail.com>
2026-02-17 08:12:46 +0530

    17feb2026

---
+++ arth-operatop.php
@@ -0,0 +1,41 @@
+<?php
+$a = 10.55;
+$b= 3;
+
+$c = $a+$b;
+$d = $a-$b;
+$e = $a*$b;
+$f = $a % $b;
+$g = $a ** $b;
+
+echo $c;
+echo "<br>";
+echo $d;
+echo "<br>";
+echo $e;
+echo "<br>";
+echo $f;
+echo "<br>";
+echo $g;
+echo"<br>";
+$a++;
+echo $a;
+echo"<br>";
+++$a;
+echo"<br>";
+echo $a;
+
+echo"<br>";
+$a--;
+echo $a;
+echo"<br>";
+--$a;
+echo"<br>";
+echo $a;
+
+echo"<br>";
+
+$h= ($a + $b) * 2;
+echo $h;
+
+?>
--- constant.php
+++ constant.php
@@ -1,6 +1,9 @@
 <?php
 
-define("test",50)
+define("test",50);
+// define("test",50);
 echo test;
-
+echo "<br>";
+$sum = test +20;
+echo $sum;
 ?>
