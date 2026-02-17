d1442f149c5ace03bf744d5fd31562ebd70d122f
vipinyadav81687 <vipinyadav81687@gmail.com>
2026-02-18 02:01:07 +0530

    18feb2026

---
+++ assignment-operators.php
@@ -0,0 +1,31 @@
+<?php
+
+$a = 10;
+$b = 3;
+
+$a +=$b ;
+ echo $a;
+ echo "<br>";
+
+ $a -= $b;
+  echo $a;
+ echo "<br>";
+
+  $a *= $b;
+  echo $a;
+ echo "<br>";
+
+  $a /= $b;
+  echo $a;
+ echo "<br>";
+
+  $a %= $b;
+  echo $a;
+ echo "<br>";
+
+  $a **= $b;
+  echo $a;
+ echo "<br>";
+
+
+
