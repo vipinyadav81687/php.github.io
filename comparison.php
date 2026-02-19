d68a9608d198a9ccb0d8e39e0cd758c828c0de7a
vipinyadav81687 <vipinyadav81687@gmail.com>
2026-02-19 08:16:47 +0530

    19feb2026

---
+++ comparison.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$a = 10;
+$b =  20;
+
+
+// echo $a == $b;
+// echo $a === $b;
+
+// echo $a != $b;
+// echo $a <> $b; 
+// echo $a !== $b; 
+
+// echo $a> $b;
+// echo $a < $b;
+// echo $a >= $b;
+// echo $a <= $b;
+echo $a<=> $b;
+
+?>
