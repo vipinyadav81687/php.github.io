b6974539ae4d64d97ce1121b1fd2a01bc04c3d02
vipinyadav81687 <vipinyadav81687@gmail.com>
2026-02-21 09:49:43 +0530

    21feb2027

--- if.php
+++ if.php
@@ -1,6 +1,17 @@
 <?php
 
+$a =  3;
+$b = 10;
 
+if($a < $b){
+    echo "a is smaller";
+}
+echo "<br>";
 
+if($a== $b):
+    echo "a is equal to b ";
+endif;
+echo "<br>";
+  echo "here is other statement";
 
-?>
+?> 
