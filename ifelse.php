5a483ee657b12ca0647e14f016d2fdc270b02543
vipinyadav81687 <vipinyadav81687@gmail.com>
2026-02-23 23:08:26 +0530

    23feb2026

---
+++ ifelse.php
@@ -0,0 +1,11 @@
+<?php
+$name = "Vipin Yadav";
+$gender = "male";
+
+if($gender== "male"){
+    echo "Hello Mr." . $name;
+}else{
+    echo "X is not same.";
+}
+
+?>
--- logicaloperator.php
+++ logicaloperator.php
@@ -1,7 +1,37 @@
 <?php
 
+$age = 26;
+
+// if($age >= 18 && $age <= 21 ){
+//     echo "you are eligible.";
+// }
+
+// if($age >= 18 and $age <= 21 ){
+//     echo "you are eligible.";
+// }
+
+// if($age >= 18 || $age <= 21){
+
+//     echo "you are eligible";
+
+// }
 
 
+// if($age >= 18 or  $age <= 21){
 
+//     echo "you are eligible";
+    
+// }
+
+
+// $age = 25;
+
+// if (!($age <= 18)){
+//     echo "you are eligible";
+// }
+
+if($age >= 18 xor $age <= 21 ){
+    echo "you are eligible";
+}
 
 ?>
