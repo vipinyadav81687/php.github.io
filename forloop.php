bc1b66e872050c9360d4cdf10986de71a050722e
vipinyadav81687 <vipinyadav81687@gmail.com>
2026-03-01 11:01:48 +0530

    1mar2026

---
+++ forloop.php
@@ -0,0 +1,4 @@
+<?php
+for($a=1;$a<23;$a++){
+    echo $a."vipin yadav<br>";
+}
---
+++ goto.php
@@ -0,0 +1,16 @@
+<?php
+
+echo "hello vipin yadav<br>";
+echo "hello vipin yadav<br>";
+goto abc;
+echo "hello vipin yadav<br>";
+echo "hello vipin yadav<br>";
+echo "hello vipin yadav<br>";
+echo "hello vipin yadav<br>";
+echo "hello vipin yadav<br>";
+echo "hello vipin yadav<br>";
+
+abc:
+echo"this is end label ABC:";
+
+?>
