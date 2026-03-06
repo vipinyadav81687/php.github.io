91fc9ea2da46d1b2740a466a3388e59575bfbc89
vipinyadav81687 <vipinyadav81687@gmail.com>
2026-03-06 09:54:41 +0530

    6mar2026

---
+++ associativearray.php
@@ -0,0 +1,27 @@
+<?php
+
+$age = array(
+    "bill" => 25,
+    "steve" => 28,
+    "elon" => 22
+);
+
+echo $age["bill"]."<br>";
+echo $age["steve"]."<br>";
+echo $age["elon"]."<br>";
+
+echo "<pre>";
+print_r($age);
+echo "<pre>";
+
+echo "<pre>";
+var_dump($age);
+echo "<pre>";
+
+$age ["elon"]=50;
+$age["steve"]="23";
+
+echo "<pre>";
+var_dump($age);
+echo "<pre>";
+
