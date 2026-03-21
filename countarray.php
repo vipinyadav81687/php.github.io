893a09778ad5a0f33633651468f771497fd56ba6
vipinyadav81687 <vipinyadav81687@gmail.com>
2026-03-21 15:29:08 +0530

    21mar2026

--- countarray.php
+++ countarray.php
@@ -1,4 +1,15 @@
 <?php
+$food = array(
+    'fruits' =>array('orange','banana', 'apple'),
+    'veggie' => array('carrot', 'collard', 'pea')
+);
+
+echo sizeof($food,1);
+echo "<br>";
+echo sizeof($food);
+echo "<br>";
+echo count($food['fruits'],1)
+
 
 
 ?>
---
+++ inarray.php
@@ -0,0 +1,13 @@
+<?php
+$food = array('orange','banana', 'apple','grapes');
+
+echo in_array('apple',$food);
+
+echo array_search('apple',$food);
+
+// $a = [['p','h'], ['p','r'], '0'];
+
+
+// if (in_array(array('p','h'),$a,true)){
+//     echo "find sucessfully.";
+// }else{echo "cant find";}
