ad33b35b2768e61e517c94a513a9b59a8808d5be
vipinyadav81687 <vipinyadav81687@gmail.com>
2026-03-23 08:23:33 +0530

    23mar2026

---
+++ arraypushpop.php
@@ -0,0 +1,14 @@
+<?php
+$fruit = ["orange","banana","apple","grapes"];
+
+array_pop($fruit);
+
+echo "<pre>";
+print_r($fruit);
+echo "</pre>";
+
+array_push($fruit,"pomigranate","guavava","pineappple");
+
+echo "<pre>";
+print_r($fruit);
+echo "</pre>";
